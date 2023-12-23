25561089198acefb3b026c6908793138aae1d532
Byte Astro <biswajitswain348@gmail.com>
2023-12-24 04:51:35 +0530

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import './App.css'
+import './App2.css'
+
+
+function App() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
